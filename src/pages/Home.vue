146decff1b199fde78169f78696b70272921235a
MathBeaudoin <mathisbeaudoin15@hotmail.com>
2025-08-13 22:53:37 -0400

    feat: adding toggle + sport choice

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,7 +3,44 @@
         <h1 class="text-4xl text-center">
             {{ $t("pages.home.description") }}
         </h1>
+
+        <ToggleGroup
+            v-model="activity"
+            @update:model-value="(newActivity) => preventNoSelection(newActivity as Activity)"
+            type="single"
+            class="mx-auto mt-4"
+            size="lg"
+        >
+            <ToggleGroupItem class="w-27" value="run" aria-label="Toggle running">
+                <Footprints />
+            </ToggleGroupItem>
+
+            <ToggleGroupItem class="w-27" value="bike" aria-label="Toggle bike">
+                <Bike />
+            </ToggleGroupItem>
+
+            <ToggleGroupItem class="w-27" value="swim" aria-label="Toggle swim">
+                <Waves />
+            </ToggleGroupItem>
+        </ToggleGroup>
     </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Footprints, Bike, Waves } from "lucide-vue-next";
+import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
+import { ref } from "vue";
+
+type Activity = "run" | "bike" | "swim";
+
+const activity = ref<Activity>("run");
+const previousActivity = ref<Activity>(activity.value);
+
+function preventNoSelection(newActivity: Activity) {
+    if (newActivity === undefined) {
+        activity.value = previousActivity.value;
+    } else {
+        previousActivity.value = activity.value;
+    }
+}
+</script>
